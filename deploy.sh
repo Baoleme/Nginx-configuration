e07a13ebb4d3833587d5b60255e1bd5d9b63cc7a
Andie <zchangan@163.com>
2018-04-02 19:33:47 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,8 @@
-echo hello
+ssh -i deploy_rsa root@119.29.252.110 << eeooff
+
+cd /root/baoleme-nginx
+git pull
+docker kill baoleme-server
+docker run -d --name baoleme-server --rm -v /root/baoleme-server:/server -p 8520:8520 node:alpine /bin/sh -c "ls /server && npm i /server && npm start --prefix /server"
+
+eeooff
