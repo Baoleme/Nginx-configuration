991534064668fabeadfbcda6e873d6404251fef7
Andie <zchangan@163.com>
2018-04-02 23:18:34 +0800

    add log

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,30 @@
+echo ---------------------------------------------
+echo             sending secret key
+echo ---------------------------------------------
 scp 2_api.baoleme.andiedie.cn.key root@119.29.252.110:~/baoleme-nginx/
-ssh -i deploy_rsa root@119.29.252.110 << eeooff
+echo ---------------------------------------------
+echo               secret key sent
+echo ---------------------------------------------
 
+echo ---------------------------------------------
+echo                login Server
+echo ---------------------------------------------
+ssh -i deploy_rsa root@119.29.252.110 << eeooff
 cd /root/baoleme-nginx
+echo ---------------------------------------------
+echo                git pull
+echo ---------------------------------------------
 git pull
-docker kill baoleme-nginx
+echo ---------------------------------------------
+echo              kill old container
+echo ---------------------------------------------
+docker kill baoleme-nginx || true
+echo ---------------------------------------------
+echo              start new container
+echo ---------------------------------------------
 docker run -d --name baoleme-nginx --rm -v /root/baoleme-nginx:/baoleme-nginx -p 443:443 nginx:alpine nginx -c /baoleme-nginx/nginx.conf -g "daemon off;"
 
 eeooff
+echo ---------------------------------------------
+echo                logout Server
+echo ---------------------------------------------
